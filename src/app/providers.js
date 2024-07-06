cd433a8fbf8fa61f7c4bab4ab12b84199fcd534f
muhammed rifkhan <rifkhanmuhammed17@gmail.com>
2024-07-07 01:57:21 +0530

    first commit

---
+++ src/app/providers.js
@@ -0,0 +1,7 @@
+// 'use client'
+
+// import { NextUIProvider } from '@nextui-org/react'
+
+// export default function Providers({ childeren }) {
+// 	return <NextUIProvider>{childeren}</NextUIProvider>
+// }
